928760b5be51803e7c648f7078e31c61c5c3486b
Karine do Valle Sóre <t2m.ksore@terceiros.mag.com.br>
2025-07-14 23:59:39 -0300

    Tela de login implementada

---
+++ Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+public class Login
+{
+    [BindProperty]
+    [Required(ErrorMessage = "O nome de usuário é obrigatório")]
+    public string Username { get; set; }
+
+    [BindProperty]
+    [Required(ErrorMessage = "A senha é obrigatória")]
+    public string Password { get; set; }
+
+    public string Message { get; set; }
+}
